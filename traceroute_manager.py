3e793d06fcf7044e2904973ddca73a2771fd30b8
OSIRIS <grantskipper17@gmail.com>
2018-07-31 19:03:36 +0000

    tc develop

--- traceroute_manager.py
+++ traceroute_manager.py
@@ -1,4 +1,6 @@
 from scapy.all import *
+from unis import Runtime
+from unis.models import *
 
 class Traceroute:
     def __init__(self, host=None):
@@ -17,18 +19,49 @@ class Traceroute_Manager:
 
     def __init__(self, host_rt=None, remote_rt=None):
 
-        self.host_rt = Runtime("http://iu-ps01.osris.org:8888") if host_rt is None else self.host_rt = host_rt
-        self.remote_rt = Runtime("http://iu-ps01.osris.org:8888") if remote_rt is None else self.remote_rt = remote_rt
+        if host_rt == None:
+            self.host_rt = Runtime("http://iu-ps01.osris.org:8888", name = "host")
+        else:
+            self.host_rt = host_rt
+
+        if remote_rt == None:
+            self.remote_rt = Runtime("http://iu-ps01.osris.org:8888", name = "remote")
+        else:
+            self.host_rt = remote_rt
+
+        self.tc = Traceroute()
+        
         return
     
     def _setup(self):
-
-        host_domain = self.rt.domains[0]
+        # get the local domain, this is the 'source'
+        host_domain = self.host_rt.domains[0]
         
+        target_domains = self.remote_rt.topologies[0].domains
+        target_nodes   = []
+        
+        #
+        # This mess is to handle when a resource can't be resolved. 
+        # Only an issue because we are navigating resources in multiple
+        # Runtimes on the fly. 
+        #
+        for d in range(len(target_domains)):
+            try:
+                current_domain = target_domains[d]    
+            except:
+                continue
+            for n in range(len(current_domain.nodes)):
+                try:
+                    current_node = current_domain.nodes[n]
+                    if "switch" in current_node.name:
+                        target_nodes.append(current_node)
+                except:
+                    continue
+
+        return target_nodes
 
 
 if __name__ == '__main__':
-    tc = Traceroute("wsu-sdn01-be.osris.org")
-    hops = tc.trace(tc.host)
-    print(hops)
-        
+       tcm = Traceroute_Manager()
+       target_nodes = tcm._setup()
+       print(target_nodes)
