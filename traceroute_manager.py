15416c624fcc495462ab45c4d3a479cf2ebb3e44
root <grantskipper17@gmail.com>
2018-08-07 16:16:32 +0000

    tc

--- traceroute_manager.py
+++ traceroute_manager.py
@@ -33,7 +33,12 @@ class Traceroute_Manager:
         
         return
     
-    def _setup(self):
+    '''
+        @param:pattern:string - string of a node name search pattern you are looking for in each domain.
+
+        The nodes retrieved from this setup phase will be used as targets for traceroute and pathing.
+    '''
+    def _setup(self, pattern):
         # get the local domain, this is the 'source'
         host_domain = self.host_rt.domains[0]
         
@@ -53,15 +58,30 @@ class Traceroute_Manager:
             for n in range(len(current_domain.nodes)):
                 try:
                     current_node = current_domain.nodes[n]
-                    if "switch" in current_node.name:
+                    if pattern in current_node.name:
                         target_nodes.append(current_node)
                 except:
                     continue
 
         return target_nodes
 
+    ''' 
+        @param:node:UNISNode
+
+        takes a valid Node retrieved in self._setup method and uses traceroute to get the hops to it.
+    '''
+    def trace_path(self, node):
+        try:
+            hops = self.tc.trace(node.properties.mgmtaddr)
+        except Exception as e:
+            print("Exception: ", e.__traceback__)
+            print("Unable to reach IP address of node: " + node.name + " - " + node.properties.mgmtaddr)
+        return hops
 
 if __name__ == '__main__':
        tcm = Traceroute_Manager()
-       target_nodes = tcm._setup()
-       print(target_nodes)
+       target_nodes = tcm._setup("virt")
+       for node in target_nodes:
+           print(node.to_JSON())
+           hops = tcm.trace_path(node)
+           print(hops)
